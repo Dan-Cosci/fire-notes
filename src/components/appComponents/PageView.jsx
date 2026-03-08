f75af66b29bc59b36a23d05d09ffb343c549f38f
Daniel <allinasdaniel@gmail.com>
2026-03-09 06:20:34 +0800

    note view

--- src/components/appComponents/PageView.jsx
+++ src/components/appComponents/PageView.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import useNoteStore from '../../state/NoteStore'
+import { useParams } from 'react-router-dom';
 
 const PageView = () => {
+  const { notes } = useNoteStore();
+  const params = useParams();
+
+  const id = params.id;
+  const note = notes.find((note) => note.id === id);
+
+  console.log(id);
+
+
   return (
-    <div>PageView</div>
+    <div className="noteContent">
+      <h1>{note.title}</h1>
+      <p>{note.content}</p>
+    
+    </div>
   )
 }
 

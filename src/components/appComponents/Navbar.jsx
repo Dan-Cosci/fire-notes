6e07f203bed8687f845bc9e806b1054e7135a717
Daniel <allinasdaniel@gmail.com>
2026-03-10 00:07:36 +0800

    note view

--- src/components/appComponents/Navbar.jsx
+++ src/components/appComponents/Navbar.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 
 import './Navbar.css'
+import icon from '../../assets/images/icon-large.png'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
 
   const [search,setSearch] = useState('');
+  const navigate = useNavigate();
 
   return (
     <nav className="navbar">
-      <div className="logo">
+      <div className="logo" onClick={() => navigate('/home')}>
+        <img src={icon} height={40} alt="pencil on fire" />
         <h1>NoteFlow</h1>
       </div>
       <div className="search">
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -4,18 +4,18 @@ import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Navbar from '../components/appComponents/Navbar'
 
+import './AppLayout.css'
 
 const AppLayout = () => {
   return (
-    <>
+    <div className="app-content">
       <header className='header'>
         <Navbar />
       </header>
       <main className='main'>
         <Outlet />
       </main>
-      <Footer />
-    </>
+    </div>
   )
 }
 

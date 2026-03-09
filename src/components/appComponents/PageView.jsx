80a890188f170faccc2c4a1cebb1bb5d00501e5f
Daniel <allinasdaniel@gmail.com>
2026-03-09 17:52:15 +0800

    note view

--- src/components/appComponents/PageView.jsx
+++ src/components/appComponents/PageView.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
-import useNoteStore from '../../state/NoteStore'
 import { useParams } from 'react-router-dom';
 import toast from 'react-hot-toast';
+
+import useNoteStore from '../../state/NoteStore'
+
+import './PageView.css'
 
 const PageView = () => {
   const { notes } = useNoteStore();
@@ -13,10 +16,18 @@ const PageView = () => {
   const [title, setTitle] = useState(note.title);
   const [content, setContent] = useState(note.content);
 
+  const [editableTitle, setEditableTitle] = useState(title);
+  const [editableContent, setEditableContent] = useState(content)
 
   const handleBlur = () => {
+    
+    setContent(editableContent);
+    setTitle(editableTitle);
+
+    toast.dismiss()
     const loading = toast.loading('saving note');
     setTimeout(() => {
+      console.log(title,content);
       toast.success('Note saved!', { id: loading });
     }, 1000);
   }
@@ -24,10 +35,20 @@ const PageView = () => {
 
   return (
     <div className="note-content" onBlur={handleBlur}>
-      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-      <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+      <div 
+      className="title" 
+      contentEditable 
+      suppressContentEditableWarning
+      onInput={(e) => setEditableTitle(e.currentTarget.textContent)}
+      data-placeholder='Title...'
+      >{title}</div>
 
-    
+      <div 
+      className="content" 
+      contentEditable 
+      suppressContentEditableWarning
+      onInput={(e) => setEditableContent(e.currentTarget.textContent)}
+      >{content}</div>
     </div>
   )
 }
